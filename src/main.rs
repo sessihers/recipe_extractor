8e1e5c08742e324c3dda78f486a66ac7435fbe7f
sessiher <sesarh@icloud.com>
2025-01-11 19:50:49 +0000

    handle json arrays

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use reqwest::{blocking};
+use reqwest::blocking;
 use reqwest::header::{HeaderMap,HeaderValue, ACCEPT, USER_AGENT};
-use scraper::{Selector};
+use scraper::Selector;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -21,16 +21,58 @@ struct RecipeInstruction {
 #[serde(untagged)]
 enum RecipeYield {
     Text(String),
-    Number(u8)
+    Number(u8),
+    NumberList(Vec<u8>),
+    TextList(Vec<String>)
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(untagged)]
+enum Kind {
+    Text(String),
+    List(Vec<String>)
+}
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+enum RecipeScript {
+    JSONObject(Recipe),
+    JSONArray(Vec<Recipe>)
+}
+
+type DeserializedRecipe = Result<RecipeScript, String>;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RecipeValidator {
+    #[serde(rename = "@context")]
+    context: Option<String>,
+    #[serde(rename = "@type")]
+    kind: Option<Kind>,
+}
+
+impl Recipe {
+    fn validate(&self) -> bool {
+        let expected_context = String::from("https://schema.org");
+        let expect_content_with_s = String::from("http://schema.org");
+        let expected_type = String::from("Recipe");
+        
+        if let (Some(context), Some(kind)) = (self.context.clone(), self.kind.clone()) {
+            let context_bool = context == expected_context || context == expect_content_with_s;
+            let kind_bool = match kind {
+                Kind::Text(s) => s == expected_type,
+                Kind::List(l) => l.contains(&expected_type)
+            };
+            return context_bool && kind_bool;
+        }
+        false
+    }
+}
 #[derive(Serialize, Deserialize, Debug)]
 struct Recipe {
     #[serde(rename = "@context")]
-    context: String,
+    context: Option<String>,
     #[serde(rename = "@type")]
-    kind: String,
+    kind: Option<Kind>,
     #[serde(rename = "cookTime")]
     cook_time: Option<String>,
     #[serde(rename = "datePublished")]
@@ -47,7 +89,7 @@ struct Recipe {
 }
 
 fn main() {
-    let url = "https://www.bbcgoodfood.com/recipes/spaghetti-meatballs";
+    let url = "https://www.allrecipes.com/beef-birria-ramen-recipe-8749389";
 
     let mut headers = HeaderMap::new();
     headers.insert(ACCEPT, HeaderValue::from_str("text/html").unwrap());
@@ -65,19 +107,39 @@ fn main() {
     let mut ld_scripts = document.select(&selector);
 
     
-    let recipe_string = ld_scripts.find(|script| {
-        let html = script.inner_html();        
-        let deserialised: Recipe = serde_json::from_str(&html).expect("cant deserialise");
-        if deserialised.context == "https://schema.org" && deserialised.kind == "Recipe" {
-            return true;
-        } 
-        false
-    }).expect("No recipe found").inner_html();
+    
+    let recipe = ld_scripts.find_map(|script| {
+        let html = script.inner_html();
+            match serde_json::from_str(&html) {
+            Ok(recipe_script) => {
+                match recipe_script {
+                   RecipeScript::JSONArray(recipe_scripts) =>  {
+                        for recipe_script in recipe_scripts {
+                            if recipe_script.validate() {
+                                return Some(recipe_script);
+                            }
+                        }
+                        return None;
+                    }
+                    RecipeScript::JSONObject(recipe_script) => {
+                        if recipe_script.validate() { 
+                            return Some(recipe_script);
+                        }
+                        return None;
+                    }
+                }
+            }
+            Err(error) => {
+                println!("Our: {error}");
+                return None;
+            }
+        }
+    });
     
 
-    let recipe: Recipe = serde_json::from_str(&recipe_string).expect("cant deserialise");
+    // let recipe: Recipe = serde_json::from_str(&recipe_string).expect("cant deserialise {recipe_string}");
 
-    println!("{:#?}", recipe);
+    println!("recipe {:#?}", recipe);
 
 
 
